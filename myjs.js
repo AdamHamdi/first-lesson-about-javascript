60039e7f9214a496105f1ce9bdf50567d1789994
Adam Hamdi <prodev.webdesign@gmail.com>
2020-06-09 15:42:21 +0100

    first commit in javascript

---
+++ myjs.js
@@ -0,0 +1,28 @@
+//POR LES BALises qui n'ont pas de balise de fermeture
+document.getElementById("txt").value = "Adam";
+document.getElementById("txt2").value = "Hamdi";
+//innerHTML button h1 h2 p <a ></a> pour les balises qui ont une balise de fermeture
+document.getElementById("btn").innerHTML = "login";
+document.getElementById("btn2").innerHTML = "Register";
+// variables
+var name = "adam";
+var age = 27;
+var v = true;
+document.write(typeof(name));
+var person = { num: 1, name: "adam", salary: 2500 };
+document.write("<br>");
+document.write(person.salary + " " + name);
+
+function marriage() {
+    var m = window.txt.value;
+    var m1 = window.txt2.value;
+    document.getElementById('lbl').innerHTML = (m + " " + "will" + " " +
+        "marry" + " " + m1);
+
+
+}
+
+function showdata(name, textbox) {
+    document.getElementById('lbl').innerHTML = 'hello' + "  " + name;
+    textbox.value = 'done';
+}
